374e6ec2263decc496758e9a7f1d110cebffa145
Mihai Vladu <mihai@academicmerit.com>
2018-04-13 17:27:36 +0300

    Making progress...

--- src/js/index.js
+++ src/js/index.js
@@ -1,80 +1,45 @@
 import '../scss/index.scss';
 
-$(document).ready(function () {
-    $('a[href^="#"]').on('click', function () {
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+$(function () {
+  $('a[href^="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+  const controller = new ScrollMagic.Controller();
+  const slides = new TimelineMax()
+    .to('#slideContainer', 1, {
+      x: '-20%',
+      ease: Linear.easeNone
+    })
+    .to('#slideContainer', 0.5, {
+      delay: 1
+    })
+    .to('#slideContainer', 1, {
+      x: '-40%',
+      ease: Linear.easeNone
+    })
+    .to('#slideContainer', 0.5, {
+      delay: 1
+    })
+    .to('#slideContainer', 1, {
+      x: '-60%',
+      ease: Linear.easeNone
+    })
+    .to('#slideContainer', 0.5, {
+      delay: 1
+    })
+    .to('#slideContainer', 1, {
+      x: '-80%',
+      ease: Linear.easeNone
     });
 
-    $(window).bind('mousewheel', function (e) {
-        const windowScroll = $(this).scrollTop();
-
-        if (e.originalEvent.wheelDelta > 0) {
-            // Mouse wheel UP
-            $($('.main-section').get().reverse()).each(function () {
-                const element = this;
-                const elementScrollTop = $(element).offset().top;
-                const elementScrollLeft = $(element).scrollLeft();
-
-                if (windowScroll === elementScrollTop) {
-                    if ($(element).find('.secondary-section').length) {
-                        console.log($($(element).find('.secondary-section').get().reverse()));
-                        $($(element).find('.secondary-section').get().reverse()).each(function () {
-                            const childScrollLeft = $(this).offset().left;
-                            console.log('elementScrollLeft: ' + elementScrollLeft + ' childScrollLeft: ' + childScrollLeft);
-
-                            if ((elementScrollLeft > childScrollLeft)) {
-                                $(element).animate({
-                                    scrollLeft: elementScrollLeft - 1024
-                                }, 500);
-
-                                return false;
-                            }
-                        });
-
-                        return false;
-                    }
-                } else if (windowScroll > elementScrollTop) {
-                    $('html, body').animate({
-                        scrollTop: elementScrollTop
-                    }, 500);
-
-                    return false;
-                }
-            });
-        } else {
-            // Mouse wheel DOWN
-            $('.main-section').each(function () {
-                const element = this;
-                const elementScrollTop = $(element).offset().top;
-                const elementScrollLeft = $(element).scrollLeft();
-
-                if (windowScroll === elementScrollTop) {
-                    if ($(element).find('.secondary-section').length) {
-                        $(element).find('.secondary-section').each(function () {
-                            const childScrollLeft = $(this).offset().left;
-                            console.log('elementScrollLeft: ' + elementScrollLeft + ' childScrollLeft: ' + childScrollLeft);
-
-                            if ((elementScrollLeft < childScrollLeft) || (elementScrollLeft > 0 && childScrollLeft === 0)) {
-                                $(element).animate({
-                                    scrollLeft: elementScrollLeft < childScrollLeft ? childScrollLeft : elementScrollLeft * 2
-                                }, 500);
-
-                                return false;
-                            }
-                        });
-
-                        return false;
-                    }
-                } else if (windowScroll < elementScrollTop) {
-                    $('html, body').animate({
-                        scrollTop: elementScrollTop
-                    }, 500);
-
-                    return false;
-                }
-            });
-        }
-    });
+  new ScrollMagic.Scene({
+    triggerElement: '#finetune-section-slider',
+    triggerHook: 'onLeave',
+    duration: '800%'
+  }).setPin('#finetune-section-slider')
+    .setTween(slides)
+    .addTo(controller);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,97 @@
 const path = require('path');
 const combineLoaders = require('webpack-combine-loaders');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const $ = require("jquery");
+const $ = require('jquery');
 
 const baseConfig = {
-    entry: [
-        './src/js'
+  entry: [
+    './src/js'
+  ],
+  output: {
+    path: '/dist',
+    publicPath: '/',
+    filename: 'bundle-[hash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body'
+    }),
+    new CopyWebpackPlugin([{
+      from: './assets',
+      to: 'assets'
+    }]),
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    })
+  ],
+  resolve: {
+    modules: [
+      '/home/devusr/node_modules',
+      './src'
     ],
-    output: {
-        path: '/dist',
-        publicPath: '/',
-        filename: 'bundle-[hash].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            inject: 'body'
-        }),
-        /*new CopyWebpackPlugin([{
-            from: './assets',
-            to: 'assets'
-        }])*/
-        new webpack.ProvidePlugin({
-            $: "jquery"
-        })
-    ],
-    resolve: {
-        modules: [
-            '/home/devusr/node_modules',
-            './src'
-        ],
-    },
-    resolveLoader: {
-        modules: ['/home/devusr/node_modules', './src']
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loaders: [
-                'babel-loader'
-            ],
-            include: [
-                path.join(__dirname, 'src')
-            ]
-        }, {
-            test: /\.svg/,
-            loader: 'url-loader?limit=100000'
-        }, {
-            test: /\.css$/,
-            loader: combineLoaders([
-                {
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader',
-                    query: {
-                        modules: true,
-                        localIdentName: '[name]__[local]___[hash:base64:5]'
-                    }
-                }
-            ])
-        }, {
-            test: /\.scss$/,
-            use: [{
-                loader: 'style-loader' // creates style nodes from JS strings
-            }, {
-                loader: 'css-loader', // translates CSS into CommonJS
-            }, {
-                loader: 'sass-loader' // compiles Sass to CSS
-            }]
-        }, {
-            test: /\.(png|jp(e*)g|svg)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8000, // Convert images < 8kb to base64 strings
-                    name: 'images/[hash]-[name].[ext]'
-                }
-            }]
-        }, {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'url-loader?name=/fonts/[name].[ext]'
-        }]
+    alias: {
+      prosemirror: 'prosemirror/dist',
+      assets: path.resolve(__dirname, 'assets')
     }
+  },
+  resolveLoader: {
+    modules: ['/home/devusr/node_modules', './src']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loaders: [
+        'babel-loader'
+      ],
+      include: [
+        path.join(__dirname, 'src')
+      ]
+    }, {
+      test: /\.svg/,
+      loader: 'url-loader?limit=100000'
+    }, {
+      test: /\.css$/,
+      loader: combineLoaders([
+        {
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      ])
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader' // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader' // translates CSS into CommonJS
+      }, {
+        loader: 'sass-loader' // compiles Sass to CSS
+      }]
+    }, {
+      test: /\.(png|jp(e*)g|svg)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8000, // Convert images < 8kb to base64 strings
+          name: 'images/[hash]-[name].[ext]'
+        }
+      }]
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      loader: 'url-loader?name=/fonts/[name].[ext]'
+    }]
+  }
 };
 
 module.exports = function (env) {
-    const envConfig = require(`./config/webpack.${env}.js`);
-    return merge(baseConfig, envConfig);
+  const envConfig = require(`./config/webpack.${env}.js`);
+  return merge(baseConfig, envConfig);
 };
