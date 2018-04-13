b62e3f1fdb425ab0e4e1e63437e63fba84fd0ce3
Mihai Vladu <mihai@academicmerit.com>
2018-04-13 11:22:05 +0300

    Updated horizontal scroll.

--- src/js/index.js
+++ src/js/index.js
@@ -1,72 +1,80 @@
 import '../scss/index.scss';
 
 $(document).ready(function () {
-  $('a[href^="#"]').on('click', function () {
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-  });
+    $('a[href^="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 
-  /*$(window).scroll(function () {
-      $('.h-scroll').each(function () {
-          console.log($(this).offset().top, $(window).scrollTop());
-      });
-  });*/
+    $(window).bind('mousewheel', function (e) {
+        const windowScroll = $(this).scrollTop();
 
-  $(window).bind('mousewheel', function (e) {
-    const windowScroll = $(this).scrollTop();
+        if (e.originalEvent.wheelDelta > 0) {
+            // Mouse wheel UP
+            $($('.main-section').get().reverse()).each(function () {
+                const element = this;
+                const elementScrollTop = $(element).offset().top;
+                const elementScrollLeft = $(element).scrollLeft();
 
-    if (e.originalEvent.wheelDelta > 0) {
-      // Mouse wheel UP
-      $($('.main-section').get().reverse()).each(function () {
-        const elementScroll = $(this).offset().top;
+                if (windowScroll === elementScrollTop) {
+                    if ($(element).find('.secondary-section').length) {
+                        console.log($($(element).find('.secondary-section').get().reverse()));
+                        $($(element).find('.secondary-section').get().reverse()).each(function () {
+                            const childScrollLeft = $(this).offset().left;
+                            console.log('elementScrollLeft: ' + elementScrollLeft + ' childScrollLeft: ' + childScrollLeft);
 
-        if (windowScroll === elementScroll) {
-          if ($(this).find('.secondary-section').length) {
-            console.log('are elemente!');
-            return false;
-          }
-        } else if(windowScroll > elementScroll) {
-          $('html, body').animate({
-            scrollTop: elementScroll
-          }, 500);
+                            if ((elementScrollLeft > childScrollLeft)) {
+                                $(element).animate({
+                                    scrollLeft: elementScrollLeft - 1024
+                                }, 500);
 
-          return false;
-        }
-      });
-    } else {
-      // Mouse wheel DOWN
-      $('.main-section').each(function () {
-        const element = this;
-        const elementScrollTop = $(element).offset().top;
-        const elementScrollLeft = $(element).scrollLeft();
+                                return false;
+                            }
+                        });
 
-        if (windowScroll === elementScrollTop) {
-          if ($(element).find('.secondary-section').length) {
-            $(element).find('.secondary-section').each(function() {
-              const childScrollLeft = $(this).offset().left;
-              console.log(elementScrollLeft, childScrollLeft);
+                        return false;
+                    }
+                } else if (windowScroll > elementScrollTop) {
+                    $('html, body').animate({
+                        scrollTop: elementScrollTop
+                    }, 500);
 
-              if(elementScrollLeft < childScrollLeft) {
-                console.log(childScrollLeft);
-                $(element).animate({
-                  scrollLeft: childScrollLeft
-                }, 500);
-
-                return false;
-              }
+                    return false;
+                }
             });
+        } else {
+            // Mouse wheel DOWN
+            $('.main-section').each(function () {
+                const element = this;
+                const elementScrollTop = $(element).offset().top;
+                const elementScrollLeft = $(element).scrollLeft();
 
-            return false;
-          }
-        } else if (windowScroll < elementScrollTop) {
-          $('html, body').animate({
-            scrollTop: elementScrollTop
-          }, 500);
+                if (windowScroll === elementScrollTop) {
+                    if ($(element).find('.secondary-section').length) {
+                        $(element).find('.secondary-section').each(function () {
+                            const childScrollLeft = $(this).offset().left;
+                            console.log('elementScrollLeft: ' + elementScrollLeft + ' childScrollLeft: ' + childScrollLeft);
 
-          return false;
+                            if ((elementScrollLeft < childScrollLeft) || (elementScrollLeft > 0 && childScrollLeft === 0)) {
+                                $(element).animate({
+                                    scrollLeft: elementScrollLeft < childScrollLeft ? childScrollLeft : elementScrollLeft * 2
+                                }, 500);
+
+                                return false;
+                            }
+                        });
+
+                        return false;
+                    }
+                } else if (windowScroll < elementScrollTop) {
+                    $('html, body').animate({
+                        scrollTop: elementScrollTop
+                    }, 500);
+
+                    return false;
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
